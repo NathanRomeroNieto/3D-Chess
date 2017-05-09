c92e3429fb5961ef3656b41e00770485d923bc77
NathanRomeroNieto <itcj.romero.nathan@gmail.com>
2017-05-09 01:06:32 -0600

    Update pieces.h

--- pieces.h
+++ pieces.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 
+//Definiciones de los valores de las columnas del tablero 
 #define A 0
 #define B 1
 #define C 2
@@ -13,66 +14,87 @@
 #define F 5
 #define G 6
 #define H 7
-
+//Definiciones del valor del turno
 #define BLACK 1337
 #define WHITE 6969
-
+//Declaracion del estado del juego
 enum GAMESTATE : int{
     MENU,WHITE_TURN,BLACK_TURN,WHITE_WIN,BLACK_WIN,TIE
 };
 
-//global variables
-extern int grid_pieces[8][8],grid_row, grid_col,highlighted_tiles[56][2];
-extern char column[9],grid_column;
+//Variables Globales
+	 
+extern int grid_pieces[8][8], grid_row, grid_col, highlighted_tiles[56][2]; //No se porque es 56*2
+extern char column[9], grid_column;
 extern GAMESTATE gamestate; //enum which stores current state, like player turns and menu
 extern GLuint p,f,v; //program, frag shader, vert shader
 
-//gloval functions
+//Funciones Globales
 extern void setShaders(void);
 extern void drawGrid(void);
 extern void initDLs(void);
 extern void clearMovesList(void);
+extern std::vector<Piece*> pieces,side_pieces;
+extern Piece* piece_at(int col, int row);
+extern void remove_piece(int col, int row);
+
+
+//Clase pieza es una clase abstracta 
 
 class Piece{
     public:
-        int c_Row, c_Col, color, textureNumber, value, numVerts; //c_Row = current row c_Col = current column
-        char c_Column;
-        GLfloat *vertexArray,*normalArray,*uvArray;
-        float var_swag;
-	    bool picked = false, en_passant = false, has_moved = false;
+	//Atributos
+		//c_ = Current 
+		int c_Row, c_Col, color, textureNumber, value, numVerts; 
+		char c_Column;
 
+		//Para entender como funcionan:
+		//http://www.songho.ca/opengl/gl_vertexarray.html#
+		//uvArray = http://www.c-jump.com/bcc/common/Talk3/OpenGL/Wk07_texture/const_images/texturemapping.png
+		GLfloat *vertexArray,*normalArray,*uvArray;
+		float var_swag;
+	//Fin Atributos
+	
+	bool picked = false, en_passant = false, has_moved = false;
+	//explicit evita las conversiones de los tipos
+	//Constructor Clases pieza
         explicit Piece(const char* modelFile, const char* textureFile,int textureNum,char col, int row);
+	
         ~Piece(){free(vertexArray);free(normalArray);free(uvArray);};
-
+	
+	//Metodos virtuales 
         virtual void draw(bool side_piece = false, float x = 0.0f, float y = 0.0f);
-	    virtual void listMoves(void);
+	virtual void listMoves(void);
         virtual void move(unsigned int col, unsigned int row);
         virtual void move(char col, unsigned int row);
-
-	    void pick(void);
-	    void unpick(void);
+	//Fin de Metodos virtuales
+	
+	//Metodos 
+	void pick(void);
+        void unpick(void);
         void createMoveList(int col_inc, int row_inc, int min_array);
         void render(void);
 };
 
-extern std::vector<Piece*> pieces,side_pieces;
-
-extern Piece* piece_at(int col, int row);
-extern void remove_piece(int col, int row);
-
+//Clase Rey Herreda de la clase Pieza
 class King : public Piece{
     public:
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         King(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("King loaded\n");
             value = 9999;
         };
         ~King();
+	
+	//Metodos
         void listMoves(void);
         void move(unsigned int col, unsigned int row);
 };
 
+//Clase Reina Herreda de la clase Pieza
 class Queen : public Piece{
     public:
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         Queen(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("Queen loaded\n");
             value = 9;
@@ -81,8 +103,10 @@ class Queen : public Piece{
         void listMoves(void);
 };
 
+//Clase Torre Herreda de la clase Pieza
 class Rook : public Piece{
     public:
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         Rook(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("Rook loaded\n");
             value = 5;
@@ -91,8 +115,10 @@ class Rook : public Piece{
         void listMoves(void);
 };
 
+//Clase Caballo Herreda de la clase Pieza
 class Knight : public Piece{
     public:
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         Knight(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("Knight loaded\n");
             value = 3;
@@ -103,8 +129,10 @@ class Knight : public Piece{
         void listMoves(void);
 };
 
+//Clase Alfil Herreda de la clase Pieza
 class Bishop : public Piece{
     public:
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         Bishop(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("Bishop loaded\n");
             value = 3;
@@ -113,10 +141,11 @@ class Bishop : public Piece{
         void listMoves(void);
 };
 
+//Clase Peon Herreda de la clase Pieza
 class Pawn : public Piece{
     public:
         bool firstMove = true;
-
+	//Pasar Parametros del contructor de la clase Rey al constructor de la clase pieza
         Pawn(const char* modelFile, const char* textureFile,int textureNum,char col, int row) : Piece(modelFile, textureFile, textureNum,col,row){
             printf("Pawn loaded\n");
             value = 1;
